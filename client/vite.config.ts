46045f7d77e68049185164f8696e387d119b4e50
bonkman1232 <pr1c3.mahmud2@gmail.com>
2026-01-02 21:07:20 +0600

    chunked code for reduced bundle size

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,4 +19,21 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor_react';
+            if (id.includes('react-router-dom')) return 'vendor_router';
+            if (id.includes('lucide-react')) return 'vendor_icons';
+            if (id.includes('recharts')) return 'vendor_recharts';
+            if (id.includes('embla-carousel-react')) return 'vendor_carousel';
+            return 'vendor_misc';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 700,
+  },
 })
